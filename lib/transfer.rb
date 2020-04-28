6f7d62e965fcc40abb9b56a6cae33218b11d95a2
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-28 13:45:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,8 +1,8 @@
 class Transfer
-  attr_accessor :from, :to, :amount
+  attr_accessor :sender, :to, :amount
   
-  def initialize (from, to, amount)
-    @from = from
+  def initialize (sender, to, amount)
+    @sender = sender
     @to = to 
     @amount = amount
   end
