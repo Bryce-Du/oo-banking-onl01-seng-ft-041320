ebb091135e69356b47f57ef43aefa276a418d96f
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-28 13:49:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,8 +13,10 @@ class Transfer
   end
   
   def execute_transaction
-    @sender.deposit(-amount)
-    @receiver.deposit(amount)
-    @status = "complete"
+    if @status == "pending"
+      @sender.deposit(-amount)
+      @receiver.deposit(amount)
+      @status = "complete"
+    end
   end
 end
